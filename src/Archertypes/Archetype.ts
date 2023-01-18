9224a34ccefeb8db950d2b535d6694049e83edb4
Rafael Brandão <rafaelbrandao1992@gmail.com>
2023-01-18 17:34:20 -0300

    Requirement 04 to test

--- src/Archertypes/Archetype.ts
+++ src/Archertypes/Archetype.ts
@@ -1,13 +1,21 @@
 import { EnergyType } from '../Energy';
 
 export default abstract class Archetype {
-  constructor(
-    public readonly name:string,
-    public readonly special: number = 0,
-    public readonly cost: number = 0,
-  ) {
-    this.name = name;
+  private readonly _name: string;
+  private readonly _special: number;
+  private readonly _cost: number;
+  
+  constructor(name:string) {
+    this._name = name;
+    this._special = 0;
+    this._cost = 0;
   }
+  
+  get name():string { return this._name; }
+
+  get special():number { return this._special; }
+
+  get cost():number { return this._cost; }
 
   static createdArchetypeInstances():number {
     throw new Error('Not implemented');
